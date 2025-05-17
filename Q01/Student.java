a7dc484466af0273cf37396f8b9d4254947d5d4e
Samudini Gunarathna <77316805+samudinigunarathna@users.noreply.github.com>
2025-05-17 10:16:21 +0000

    Update Student class

---
+++ Q01/Student.java
@@ -0,0 +1,42 @@
+class Student{
+    private int studentid;
+    private String name;
+    private int dayAttended;
+
+    void Student(int studentid, String name, int dayAttended){
+        this.studentid=studentid;
+        this.name=name;
+        this.dayAttended=dayAttended;
+    }
+
+    void SetStudentid(int studentid){
+        this.studentid=studentid;
+    }
+    void SetName(String name){
+        this.name=name;
+    }
+    void SetDayAttended(int dayAttended){
+        this.dayAttended=dayAttended;
+    }
+    
+    int Studentid(){
+        return studentid;
+    }
+    String Name(){
+        return name;
+    }
+    int DayAttended(){
+        return dayAttended;
+    }
+
+    void Display(){
+        System.out.println("StudentID: "+studentid+" Name: "+name+" Days Attended: "+dayAttended);
+    }
+}
+
+class Main{
+    public static void main(String[] args){
+        Student st=new Student();
+        st.Display();
+    }
+}
