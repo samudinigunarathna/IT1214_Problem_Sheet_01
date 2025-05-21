8c6a0aa18def30f437b238c231c72b1f143de600
Samudini Gunarathna <77316805+samudinigunarathna@users.noreply.github.com>
2025-05-21 05:36:46 +0000

    Created void Method

--- Q02/Vehicle.java
+++ Q02/Vehicle.java
@@ -3,10 +3,15 @@ class Vehicle{
     private String ownerName;
     private int hoursParked;
 
+    void Vehicle(String licensePlate, String ownerName, int hoursParked){
+      this.licensePlate=licensePlate;
+      this.ownerName=ownerName;
+      this.hoursParked=hoursParked;  
+    }
 }
 
 class App{
     public static void main(String[] args){
-        
+
     }
 }
