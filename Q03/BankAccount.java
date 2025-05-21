4f68b522629d1f4219e4108a3a6d935843cc6fa5
Samudini Gunarathna <77316805+samudinigunarathna@users.noreply.github.com>
2025-05-21 06:11:34 +0000

    Created BankAccount class and App class

---
+++ Q03/BankAccount.java
@@ -0,0 +1,11 @@
+class BankAccount{
+    private int accountNumber;
+    private String accountHolder;
+    private double balance;
+}
+
+class App{
+    public static void main(String[] args){
+        
+    }
+}
